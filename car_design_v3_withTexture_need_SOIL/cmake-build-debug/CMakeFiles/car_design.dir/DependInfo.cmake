a38df5b95dadbe123fb02eb444708b23a26a36da
Riddy1907 <ridvankina50@gmail.com>
2022-06-23 00:39:46 +0200

    Add files via upload

---
+++ car_design_v3_withTexture_need_SOIL/cmake-build-debug/CMakeFiles/car_design.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ridvan/dev/CLionProjects/GDV_Projekte/car_design_v3_withTexture_need_SOIL/car.cpp" "/Users/ridvan/dev/CLionProjects/GDV_Projekte/car_design_v3_withTexture_need_SOIL/cmake-build-debug/CMakeFiles/car_design.dir/car.cpp.o"
+  "/Users/ridvan/dev/CLionProjects/GDV_Projekte/car_design_v3_withTexture_need_SOIL/main.cpp" "/Users/ridvan/dev/CLionProjects/GDV_Projekte/car_design_v3_withTexture_need_SOIL/cmake-build-debug/CMakeFiles/car_design.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ car_design_v3_withTexture_need_SOIL/cmake-build-debug/CMakeFiles/car_design.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_design.dir/car.cpp.o"
+  "CMakeFiles/car_design.dir/main.cpp.o"
+  "car_design"
+  "car_design.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car_design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
